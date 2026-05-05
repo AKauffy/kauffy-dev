1ab69d37d8c9b8a9e8075dc96f8295c5f6f32f82
akauffy <aak105@pitt.edu>
2026-05-05 17:35:27 -0400

    Init

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="dns-prefetch" href="https://portfolio.kauffy.com" />
+      <link
+        rel="preconnect"
+        href="https://portfolio.kauffy.dev"
+        crossOrigin="anonymous"
+      />
+    </>
+  );
+}
